b16762621a00536ffcc34c5c74672a2d141e5552
polvnco <cpolanco000@gmail.com>
2021-06-05 12:16:55 -0400

    testing Git

---
+++ src/main/java/Assignment1/example/ex06.java
@@ -0,0 +1,30 @@
+package Assignment1.example;
+
+
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class ex06
+{
+    public static void main( String[] args )
+    {
+        System.out.print( "What is your current age? " );
+        Scanner currentAge = new Scanner(System.in);
+        String age = currentAge.nextLine();
+        int x = Integer.parseInt(age);
+
+        System.out.print( "At what age would you like to retire? " );
+        Scanner futureAge = new Scanner(System.in);
+        String retirement = futureAge.nextLine();
+        int y = Integer.parseInt(retirement);
+
+        y = y - x;
+
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        int retirementYear = year + y;
+
+        System.out.println("You have " + y +" years left until you can retire.");
+        System.out.println("It's " + year + ", so you can retire in " + retirementYear + ".");
+
+    }
+}
